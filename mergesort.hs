d2b684fd973182b25500c22364b232afad61e68b
Shine Wang <shine@getbeyond.me>
2015-10-21 01:40:42 -0400

    cleaned up mergesort

--- mergesort.hs
+++ mergesort.hs
@@ -1,3 +1,4 @@
+-- Theta(m+n)
 merge :: Ord a => [a] -> [a] -> [a]
 merge x [] = x
 merge [] x = x
@@ -6,13 +7,17 @@ merge (x:xs) (y:ys) =
   then x : merge xs (y:ys)
   else y : merge (x:xs) ys
 
+-- Theta(n log n)
 mergeSort :: Ord a => [a] -> [a]
 mergeSort [] = []
 mergeSort [x] = [x]
 mergeSort xs =
   let
-    len = length xs
-    first = take (len `div` 2) xs
-    second = drop (len `div` 2) xs
+    halfLen = ((`div` 2) . length) xs
+    front = take halfLen xs
+    back = drop halfLen xs
   in
-    merge (mergeSort first) (mergeSort second)
+    merge (mergeSort front) (mergeSort back)
+
+main :: IO ()
+main = putStrLn $ show $ mergeSort [2, 3, 4, 1, 5]
